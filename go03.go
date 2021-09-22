647ebf6bcafcda01fe7f650c8f324e370bd499fc
leikang123 <1013626230@qq.com>
2021-09-22 16:57:04 +0800

    demo

---
+++ go03.go
@@ -0,0 +1,24 @@
+//指针类型学习
+package main
+import "fmt"
+func main(){
+var a *int
+fmt.Println("a=",a)
+num :=1
+num1 :=2
+fmt.Println("num=",num,"num1=",num1)
+var q *int
+var p *int
+q =&num1
+p =&num
+fmt.Println("q=",&q)
+fmt.Println("num=",p)
+fmt.Println("P=",&p)
+*p=10
+fmt.Println("p=",*p)
+var x *int
+x =new(int)
+fmt.Println("x=",x)
+*x =12
+fmt.Println("*x=",*x)
+}
