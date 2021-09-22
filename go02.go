be31b9611e2f54461c95aa4ec472b21b39a68247
leikang123 <1013626230@qq.com>
2021-09-22 16:55:06 +0800

    demo

---
+++ go02.go
@@ -0,0 +1,18 @@
+package main
+import "fmt"
+func main(){
+x,y :=7,9
+fmt.Println(" x + y ",x+y)
+fmt.Println(" x - y ",x-y)
+fmt.Println(" x * y ",x*y)
+fmt.Println(" x / y ",x/y)
+fmt.Println(" x % y ",x%y)
+isbool :=true
+hate :=false
+fmt.Println(isbool && hate)
+fmt.Println(isbool || hate)
+fmt.Println(isbool)
+a,b,c :="A","中国",98
+fmt.Println("a=",a,"b=",b,"c=",c)
+}
+
