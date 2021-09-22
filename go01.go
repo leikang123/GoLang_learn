ab8893193eb95f460cc276def4b2c392f881e442
leikang123 <1013626230@qq.com>
2021-09-22 16:54:24 +0800

    demo

---
+++ go01.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main(){
+var a int = 5
+var b float32 = 4.32
+//var c double = 5.7
+//var d char ='A'
+const pi float64 = 3.1456789
+
+//x,y=14,15
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println(c)
+//fmt.Println(d)
+//fmt.Println("x=",x,"y=",y)
+}
