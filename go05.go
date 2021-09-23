22802609e7b702388f45d1ff15351daafc6f0416
leikang123 <1013626230@qq.com>
2021-09-23 17:36:36 +0800

    demo

---
+++ go05.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+func main(){
+age :=18;
+switch age {
+  case 16: fmt.Println("learn is syude")
+  case 18: fmt.Println(" largr is dhj")
+  case 19: fmt.Println("hgf")
+  case 20: fmt.Println("you should go ")
+  default: fmt.Println(" are you")
+}
+}
