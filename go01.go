04ca4c9dc03d9214504935efa4ac17da667b6e2b
leikang123 <1013626230@qq.com>
2021-10-01 19:13:50 +0800

    update demo

--- go01.go
+++ go01.go
@@ -1,10 +1,12 @@
 package main
 import "fmt"
 func main(){
+// 定义一个变量 a ,b 语法格式：var 变量名 变量类型 = 
 var a int = 5
 var b float32 = 4.32
 //var c double = 5.7
 //var d char ='A'
+// const 变量名 变量类型 定义常量
 const pi float64 = 3.1456789
 
 //x,y=14,15
