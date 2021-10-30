0aa8b9875616325017a5a94ed4505376010b7c97
leikang123 <1013626230@qq.com>
2021-10-30 21:03:53 +0800

    demo

---
+++ gofunc7-1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func addSub(x int, y int )  (sum int + sub int){
+
+	// 两个变量的计算
+	sum = x + y
+	sub = x -y
+	 return sum + sub 
+
+}
+
+func main (){
+	a :=4
+	b :=9
+	f2 := addSub
+	sum,sub := f2(a,b)
+	fmt.Println(a,"+",b,"=",sum)
+	fmt.Println(a,"-",b,"=",sub)
+}
