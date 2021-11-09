8eacea0fc7d451d94faad3a47859916b9e49a67b
leikang123 <1013626230@qq.com>
2021-11-09 11:41:54 +0800

    demo

---
+++ struct6.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	title  string
+	author string
+	id     int
+	num    int
+}
+type Book1 struct {
+	Book
+	price int
+}
+type Book2 struct {
+	Book
+	bookSize int
+}
+
+func main() {
+	b := &Book{}
+	b1 := &Book1{}
+	b2 := &Book2{}
+	fmt.Println("b:", b)
+	fmt.Println("b1:", b1)
+	fmt.Println("b2:", b2)
+
+}
