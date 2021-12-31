1dc71b9523bc669445e1fc02ef4407171d3cb0c6
leikang123 <1013626230@qq.com>
2021-12-31 18:04:55 +0800

    update

---
+++ thread01.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func task1() {
+	for {
+		fmt.Println(time.Now().Format("15:09:09"), "正在处理task1的任务！")
+		time.Sleep(time.Second * 3)
+	}
+}
+func task2() {
+	for {
+		fmt.Println(time.Now().Format("15:11:13"), "正在处理task2的任务")
+		time.Sleep(time.Second * 1)
+	}
+}
+func main() {
+	go task1()
+	go task2()
+	for {
+		fmt.Println(time.Now().Format("17:01:01"), "正在处理住线程的任务")
+		time.Sleep(time.Second * 2)
+	}
+}
