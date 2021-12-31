69bc916c643ebda7a532394d6de2ca4d9475f9e4
leikang123 <1013626230@qq.com>
2021-12-31 18:06:16 +0800

    update

--- interface2.go
+++ interface2.go
@@ -29,10 +29,31 @@ func (mysql *Mysql) Disconnector() error {
 	fmt.Println("Mysql Disconnector Success!")
 	return nil
 }
+
+type Redis struct {
+	DBname string
+	Size   int
+}
+
+func (redis *Redis) Connector() error {
+	fmt.Println("redis is :", redis.DBname)
+	redis.Size = 5
+	return nil
+}
+func (redis *Redis) Disconnector() error {
+	fmt.Println("redis is suceess!")
+	return nil
+}
 func main() {
 	var mysql = Mysql{DBname: "sudent"}
 	fmt.Println("start connector")
 	mysql.Connector()
 	fmt.Println("fail connector")
 	mysql.Disconnector()
+
+	var redis = Redis{DBname: "leikang"}
+	fmt.Println("writer leikang")
+	redis.Connector()
+	fmt.Println("fsail ....")
+	redis.Disconnector()
 }
